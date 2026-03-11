a73679014eb1703e4185dd4b41f7161a6cbe0749
Ka On Leung <kleu060@gmail.com>
2026-03-11 23:41:09 +1300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/ccnb2/',
+    // base: '/ccnb2/',
     server: {
         host: 'ccnb2.test',   // your APP_URL domain
         port: 5173,
